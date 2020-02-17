27ee03b6a74a6cefe6cae102654b9f7c4632d31b
imran110219 <imran110219@gmail.com>
2020-02-18 00:15:38 +0600

    Soap

---
+++ SoapWebService/src/org/sadman/soap/IPLocationFinder.java
@@ -0,0 +1,14 @@
+package org.sadman.soap;
+
+public class IPLocationFinder {
+
+	public static void main(String[] args) {
+		if(args.length != 1)
+			System.out.println("You need to pass one IP address");
+		else {
+			String ipAddress = args[0];
+			
+		}
+	}
+
+}
