cb9371cfdbb8530e6c33e7dee7236b2cc4ae4ce2
imran110219 <imran110219@gmail.com>
2020-02-19 23:36:11 +0600

    SOAP running

--- Testmart/src/org/sadman/soap/ProductCatelog.java
+++ Testmart/src/org/sadman/soap/ProductCatelog.java
@@ -3,14 +3,28 @@ package org.sadman.soap;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import org.sadman.soap.business.ProductServiceImpl;
+
+@WebService
 public class ProductCatelog {
 	
+	ProductServiceImpl productService = new ProductServiceImpl();
+	
+	@WebMethod
 	public List<String> getProductCategories(){
-		List<String> categories = new ArrayList<>();
-		categories.add("Books");
-		categories.add("Moviess");
-		categories.add("Music");
-		return categories;
+		return productService.getProductCategories();
+	}
+	
+	@WebMethod
+	public List<String> getProducts(String category){
+		return productService.getProducts(category);
 	}
 
+	@WebMethod
+	public boolean addProducts(String category, String product) {
+		return productService.addProducts(category, product);
+	}
 }
---
+++ Testmart/src/org/sadman/soap/business/ProductServiceImpl.java
@@ -0,0 +1,61 @@
+package org.sadman.soap.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductServiceImpl {
+	List<String> bookList = new ArrayList<>();
+	List<String> musicList = new ArrayList<>();
+	List<String> movieList = new ArrayList<>();
+	
+	public ProductServiceImpl() {
+		bookList.add("Angels and Demons");
+		bookList.add("Ast of War");
+		bookList.add("War & Peace");
+		
+		movieList.add("RockStar");
+		movieList.add("Mission Impossible");
+		movieList.add("James Bond");
+		
+		musicList.add("I want it that way");
+		musicList.add("Good Bad");
+		musicList.add("Here without you");
+	}
+	
+	public List<String> getProductCategories(){
+		List<String> categories = new ArrayList<>();
+		categories.add("Books");
+		categories.add("Movies");
+		categories.add("Music");
+		return categories;
+	}
+	
+	public List<String> getProducts(String category){
+		switch (category.toLowerCase()) {
+		case "books":
+			return bookList;
+		case "music":
+			return musicList;
+		case "movies":
+			return movieList;
+		}
+		return null;
+	}
+	
+	public boolean addProducts(String category, String product) {
+		switch (category.toLowerCase()) {
+		case "books":
+			bookList.add(product);
+			break;
+		case "music":
+			musicList.add(product);
+			break;
+		case "movies":
+			movieList.add(product);
+			break;
+		default:
+			return false;
+		}
+		return true;
+	}
+}
