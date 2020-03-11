e729f2fe2772bf215a8fb383b77ef6fbb4a02cf2
Sadman <imran110219@gmail.com>
2020-03-11 18:07:44 +0600

    Singleton Pattern Complete

---
+++ DesignPattern/src/Creational/Singleton/TestSingleton.java
@@ -0,0 +1,36 @@
+package Creational.Singleton;
+
+import java.io.*;
+
+/**
+ * Created by Sadman on 3/11/2020.
+ */
+public class TestSingleton {
+    static DemoSingleton instanceOne = DemoSingleton.getInstance();
+
+    public static void main(String[] args) {
+        try {
+            // Serialize to a file
+            ObjectOutput out = new ObjectOutputStream(new FileOutputStream(
+                    "filename.ser"));
+            out.writeObject(instanceOne);
+            out.close();
+
+            instanceOne.setI(20);
+
+            // Serialize to a file
+            ObjectInput in = new ObjectInputStream(new FileInputStream(
+                    "filename.ser"));
+            DemoSingleton instanceTwo = (DemoSingleton) in.readObject();
+            in.close();
+
+            System.out.println(instanceOne.getI());
+            System.out.println(instanceTwo.getI());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
