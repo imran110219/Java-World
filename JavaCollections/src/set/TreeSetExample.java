9364316479c1fd4585b4f46ed58c6b57339d1e37
Sadman Sobhan <imran110219@gmail.com>
2020-12-03 14:13:33 +0600

    Collection code added

--- JavaCollections/src/set/TreeSetExample.java
+++ JavaCollections/src/set/TreeSetExample.java
@@ -1,7 +1,41 @@
 package set;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
 /**
  * Created by Sadman on 3/8/2020.
  */
 public class TreeSetExample {
+    public static void main(String[] args) {
+        //1. Create LinkedHashSet
+        TreeSet<String> treehSet = new TreeSet<>();
+
+        //2. Add elements to LinkedHashSet
+        treehSet.add("A");
+        treehSet.add("B");
+        treehSet.add("C");
+        treehSet.add("D");
+        treehSet.add("E");
+
+        System.out.println(treehSet);
+
+        //3. Check if element exists
+        boolean found = treehSet.contains("A");        //true
+        System.out.println(found);
+
+        //4. Remove an element
+        treehSet.remove("D");
+
+        //5. Iterate over values
+        Iterator<String> itr = treehSet.iterator();
+
+        while(itr.hasNext())
+        {
+            String value = itr.next();
+
+            System.out.println("Value: " + value);
+        }
+    }
 }
