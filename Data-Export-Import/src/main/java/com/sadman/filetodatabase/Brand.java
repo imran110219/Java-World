85b5b1593a06cf38c741177b28537cf0edd39ce1
Sadman <imran110219@gmail.com>
2021-01-11 10:07:05 +0600

    add

--- Data-Export-Import/src/main/java/com/sadman/filetodatabase/Brand.java
+++ Data-Export-Import/src/main/java/com/sadman/filetodatabase/Brand.java
@@ -8,7 +8,7 @@ import java.util.Date;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.*;
 
-public class ExcelToDatabase {
+public class Brand {
     public static void main(String[] args) {
         String jdbcURL = "jdbc:mysql://localhost:3306/medical_inventory?useSSL=false";
         String username = "root";
@@ -26,28 +26,37 @@ public class ExcelToDatabase {
 
             Workbook workbook = new XSSFWorkbook(inputStream);
 
-            Sheet firstSheet = workbook.getSheetAt(0);
+            Sheet firstSheet = workbook.getSheetAt(4);
             Iterator<Row> rowIterator = firstSheet.iterator();
 
             connection = DriverManager.getConnection(jdbcURL, username, password);
             connection.setAutoCommit(false);
 
-            String sql = "INSERT INTO company (id, name) VALUES (?,?)";
+            String sql = "INSERT INTO brand (id, name, strength, generic, company, price) VALUES (?,?,?,?,?,?)";
             PreparedStatement statement = connection.prepareStatement(sql);
 
             int count = 0;
 
 //            rowIterator.next(); // skip the header row
-
+            DataFormatter formatter = new DataFormatter();
             while (rowIterator.hasNext()) {
                 Row nextRow = rowIterator.next();
 //                Iterator<Cell> cellIterator = nextRow.cellIterator();
 
                 count++;
                 String name = nextRow.getCell(0).getStringCellValue();
+                String strength = formatter.formatCellValue(nextRow.getCell(1));
+                String generic = nextRow.getCell(2).getStringCellValue();
+                String company = nextRow.getCell(3).getStringCellValue();
+                String price = nextRow.getCell(4).getStringCellValue();
+
 
                 statement.setInt(1, count);
                 statement.setString(2, name);
+                statement.setString(3, strength);
+                statement.setString(4, generic);
+                statement.setString(5, company);
+                statement.setString(6, price);
 
                 statement.addBatch();
 
